11b6cb43ce0254184d7f09772bf1bfd51f3df3b3
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-31 03:04:20 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -9,7 +9,7 @@
     $dsn = 'mysql:host=zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=f33okin59mld7tzf';
     $username = 'hu0wlnc966uyzjy4';
     $password = 'cmtbdgfv39s2i8o1';
-    /*
+   
     try {
         //local development server connection
         //if using a $password, add it as 3rd parameter
@@ -21,5 +21,5 @@
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
         exit();
-    }*/
+    }
 ?>
